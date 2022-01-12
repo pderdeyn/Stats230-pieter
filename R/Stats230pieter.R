f5e714d71183f9efde0c17b19793df5a8b05c00a
pieter <pderdeyn1@gmail.com>
2022-01-12 15:54:06 -0800

    Create and document mat_mult.R, a function for multiplying matrices
    Attempt to create Stats230pieter.R to document whole package

---
+++ R/Stats230pieter.R
@@ -0,0 +1,9 @@
+#' Stats230pieter: A package for completing homework for UCI's Stats 230
+#'
+#' @section Stats230 functions:
+#' mat_mult
+#'
+#' @docType package
+#' @name Stats230pieter
+NULL
+#> NULL
---
+++ R/mat_mult.R
@@ -0,0 +1,21 @@
+#' Multiple two matrices and a vector
+#'
+#' @param A a square matrix
+#' @param B a square matrix with the same dim as A
+#' @param left a boolean option that determines which product is computed first
+#' @examples
+#' mat_mult(cbind(c(1,2),c(3,4)),rbind(c(1,2),(3,4)),c(1,2),True)
+#' @return matrix product A x B x x
+#' @export
+mat_mult <- function(A,B,x,left=TRUE){
+  prod <- 1
+  if (left) {
+    prod <- A %*% B
+    prod <- prod %*% x
+  }
+  else {
+    prod <- B %*% x
+    prod <- A %*% prod
+  }
+  return(prod)
+}
