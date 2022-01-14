5a597650118715db394fcaa3073ee50c18e5b40a
pieter <pderdeyn1@gmail.com>
2022-01-14 13:37:10 -0800

    Document a missing parameter for mat_mult and attempt to use latex formatting for the @return documentation

--- R/mat_mult.R
+++ R/mat_mult.R
@@ -2,10 +2,11 @@
 #'
 #' @param A a square matrix
 #' @param B a square matrix with the same dim as A
+#' @param x a vector with length equal to length of A
 #' @param left a boolean option that determines which product is computed first
 #' @examples
 #' mat_mult(cbind(c(1,2),c(3,4)),rbind(c(1,2),(3,4)),c(1,2),True)
-#' @return matrix product A x B x x
+#' @return matrix product \eqn{A \times B \times x}
 #' @export
 mat_mult <- function(A,B,x,left=TRUE){
   prod <- 1
